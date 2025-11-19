ea9b7aaccdae127388a45fe3a4e6e327e2b021f1
leaox77 <leandroeguerdo@gmail.com>
2025-11-19 00:17:54 -0400

    ✨ [ADD] Header and Main components

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,11 @@
 import Header from "./components/Header";
+import Main from "./components/Main";
 
 function App() {
   return (
     <>
       <Header />
-
-      <main className="max-w-6xl mx-auto mt-10 px-6">
-        <h1 className="text-4xl font-bold text-primary">
-          Bienvenido a Sueltito 🎉
-        </h1>
-        <p className="mt-4 text-lg text-gray-700">
-          Tu landing page ya está funcionando con Tailwind y React.
-        </p>
-      </main>
+      <Main />
     </>
   );
 }
